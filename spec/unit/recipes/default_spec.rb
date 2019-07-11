001634ec0ad3c78d5f708a580fa26c921207594b
Harish Kommineni <komhar01@CSGICORP.COM>
2019-07-11 18:46:32 -0500

    Chefspec unit tests for all steps in recipes

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -20,11 +20,33 @@ describe 'redis::default' do
     it 'updates the package repository' do
       expect(chef_run).to run_execute('apt-get update')
     end
-    it 'installs the necessary packages'
-    it 'retreives the application from source code'
-    it 'unzips the application'
-    it 'builds and installs the application'
-    it 'installs the server'
-    it 'starts yje service'
+
+    it 'installs the necessary packages' do
+      expect(chef_run).to install_package('build-essential')
+      expect(chef_run).to install_package('tcl8.5')
+    end
+
+    it 'retreives the application from source code' do
+      expect(chef_run).to create_remote_file('/tmp/redis-2.8.9.tar.gz')
+    end
+
+    it 'unzips the application' do
+      resource = chef_run.remote_file('/tmp/redis-2.8.9.tar.gz')
+      expect(resource).to notify('execute[tar xzf /tmp/redis-2.8.9.tar.gz]').to(:run).immediately
+    end
+
+    it 'builds and installs the application' do
+      resource = chef_run.execute('tar xzf /tmp/redis-2.8.9.tar.gz')
+      expect(resource).to notify('execute[make && make install]').to(:run).immediately
+    end
+
+    it 'installs the server' do
+      resource = chef_run.execute('make && make install')
+      expect(resource).to notify('execute[echo -n | ./install_server.sh]').to(:run).immediately
+    end
+
+    it 'starts the service' do
+      expect(chef_run).to start_service('redis_6379')
+    end
   end
 end
